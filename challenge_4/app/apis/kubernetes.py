ee73cdca7c5a8649eee7776b0616dd98d7805067
David Crook <david@easystockinvestor.com>
2019-05-15 15:20:03 -0400

    added secret mounting and kubectl install

--- challenge_4/app/apis/kubernetes.py
+++ challenge_4/app/apis/kubernetes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify
 import sys
 from bson.json_util import dumps
 import json
-
+from subprocess import Popen, PIPE
 
 kubernetes = Blueprint('kubernetes', __name__, url_prefix='/api/v1/k8')
 
@@ -14,3 +14,15 @@ def test():
     req_dict = request.get_json()
     print(req_dict)
     return json.dumps({"output":req_dict["input"]})
+
+@kubernetes.route("/basichealth", methods=["GET"])
+def basic_health():
+    """
+    gets some basic kubernetes health
+    """
+    process = Popen(["kubectl", "get", "pods"], stdout=PIPE)
+    (output, err) = process.communicate()
+    print(output)
+    exit_code = process.wait()
+    return json.dumps({"output" : output})
+    
